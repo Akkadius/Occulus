6d1422ed933c4a26184c587b8689508832f26cb1
Akkadius <akkadius1@gmail.com>
2019-03-04 03:08:47 -0600

    Hook up telnet service

---
+++ app/core/telnet-service.js
@@ -0,0 +1,49 @@
+const Telnet   = require('telnet-client')
+var connection = new Telnet()
+
+/**
+ * telnet-service
+ * @type {{exec: module.exports.exec}}
+ */
+module.exports = {
+
+  /**
+   * Execute command
+   *
+   * @param command
+   */
+  exec: async function (command) {
+
+    try {
+
+      /**
+       * Setup
+       *
+       * @type {{maxBufferLength: string, shellPrompt: string, port: number, host: string, timeout: number}}
+       */
+      await connection.connect(
+        {
+          host: '127.0.0.1',
+          port: 9000,
+          shellPrompt: '>',
+          timeout: 10,
+          maxBufferLength: '10M',
+        }
+      );
+
+      const response = await connection.send(
+        command,
+        {
+          waitfor: '> $',
+          maxBufferLength: '10M'
+        }
+      );
+
+      return response.replace("\n\r>", "").trim();
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  },
+};

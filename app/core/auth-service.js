0b06136245a1e804766709b5eb22839d14109ba2
Akkadius <akkadius1@gmail.com>
2019-03-05 01:25:12 -0600

    Auth

---
+++ app/core/auth-service.js
@@ -0,0 +1,14 @@
+module.exports = {
+  check: function (req, res, next) {
+
+    /**
+     * Check against session if logged in
+     */
+    if (req.session.loggedIn) {
+      next();
+      return;
+    }
+
+    res.redirect('/login');
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express  = require('express');
 var router   = express.Router();
 var template = require('../app/core/template-render');
+var auth     = require('../app/core/auth-service');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', auth.check, function (req, res, next) {
   const dashboard = template.load("dashboard").render();
 
   /**

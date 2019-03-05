19ef8f6494889d3b259e29a93af7587c42024765
Akkadius <akkadius1@gmail.com>
2019-03-04 22:57:37 -0600

    Update core.js

--- public/assets/js/app/core.js
+++ public/assets/js/app/core.js
@@ -1,4 +1,9 @@
-require(['jquery'], function ($) {
+/**
+ * core.js
+ */
+
+require(['jquery', 'bootstrap'], function ($) {
+
   $(document).ready(function () {
     $.get("/dashboard/stats", function (data) {
       $('.characters').html(kFormatter(data.characters));
@@ -9,14 +14,43 @@ require(['jquery'], function ($) {
       $('.server-longname').html(data.long_name);
       $('.server-uptime').html(data.uptime);
       document.title = document.title + " " + data.long_name;
-      //console.log(data);
     });
   });
 
+  /**
+   * modal("Test Title", "Test Body");
+   * @param title
+   * @param body
+   * @param footer
+   */
+  function modal(title, body, footer = null) {
+    if (title) {
+      $("#app-modal-title").html(title);
+    }
+
+    if (body) {
+      $("#app-modal-body").html(body);
+    }
+
+    if (footer) {
+      $("#app-modal-footer").html(footer);
+    }
+
+    $("#app-modal").modal('show');
+  }
+
+  /**
+   * @param x
+   * @returns {string}
+   */
   function commify(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
+  /**
+   * @param num
+   * @returns {string}
+   */
   function kFormatter(num) {
     return num > 999 ? (num / 1000).toFixed(1) + 'k' : num
   }

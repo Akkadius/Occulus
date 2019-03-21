014ffad23e8e44f2a2d2675125a04260d20e8b80
Akkadius <akkadius1@gmail.com>
2019-03-21 03:38:16 -0500

    Add process stats to zone servers

--- routes/zoneservers.js
+++ routes/zoneservers.js
@@ -2,30 +2,50 @@
  * zoneservers.js
  * @type {createApplication}
  */
-var express     = require('express');
-var router      = express.Router();
-var template    = require('../app/core/template-render');
-var auth        = require('../app/core/auth-service');
+let express     = require('express');
+let router      = express.Router();
+let template    = require('../app/core/template-render');
+let auth        = require('../app/core/auth-service');
 let dataService = require('../app/core/eqemu-data-service-client.js');
+let pidusage    = require('pidusage')
 
 /* GET home page. */
 router.get('/', auth.check, function (req, res, next) {
   dataService.getZoneList().then(zonelist => {
-    const content  = template.load("zoneservers").renderEjs({"zonelist": zonelist});
-    const navbar   = template.load("navbar").render();
-    const username = req.session.username
 
-    /**
-     * Response
-     */
-    res.send(
-      template
-        .load("index")
-        .var("username", username.charAt(0).toUpperCase() + username.substr(1))
-        .var("navbar", navbar)
-        .var("content", content)
-        .render()
-    );
+    let zone_pids = [];
+    zonelist.forEach(function (zone) {
+      zone_pids.push(zone.zone_os_pid);
+    });
+
+    pidusage(zone_pids, function (err, stats) {
+      const content = template
+        .load("zoneservers")
+        .renderEjs(
+          {
+            "zonelist": zonelist,
+            "process_stats": stats
+          }
+        );
+
+      const navbar   = template.load("navbar").render();
+      const username = req.session.username
+
+      /**
+       * Response
+       */
+      res.send(
+        template
+          .load("index")
+          .var("username", username.charAt(0).toUpperCase() + username.substr(1))
+          .var("navbar", navbar)
+          .var("content", content)
+          .render()
+      );
+
+    });
+
+
   });
 });
 

281dd5f59f60e8894a5b745bddc1ca7e5d6b0259
Akkadius <akkadius1@gmail.com>
2019-03-22 04:53:56 -0500

    Consolidate charting code, populate the rest of the charts

--- app/core/netstat-listener.js
+++ app/core/netstat-listener.js
@@ -148,7 +148,7 @@ module.exports = {
           }
         }
       }
-      
+
     }
   },
 
--- routes/api/zoneserver/netstat.js
+++ routes/api/zoneserver/netstat.js
@@ -7,7 +7,7 @@ let router          = express.Router();
 let moment          = require('moment');
 let netstatListener = require('../../../app/core/netstat-listener');
 
-router.get('/:port/chart/packet_types_sent', function (req, res, next) {
+router.get('/:port/chart', function (req, res, next) {
   const unix_time   = Math.floor(new Date() / 1000);
   const min_seconds = (unix_time - netstatListener.max_series_store_time);
   const port        = req.params.port;
@@ -17,7 +17,18 @@ router.get('/:port/chart/packet_types_sent', function (req, res, next) {
    */
   netstatListener.addListener(port);
 
-  let chart_columns = [];
+  let clients;
+  let packet_loss_inbound_columns  = [];
+  let packet_loss_outbound_columns = [];
+  let ping_columns                 = [];
+  let receive_bytes_columns        = [];
+  let receive_packet_type_columns  = [];
+  let resent_fragments_columns     = [];
+  let resent_non_fragments_columns = [];
+  let resent_packets_columns       = [];
+  let sent_bytes_columns           = [];
+  let sent_packet_type_columns     = [];
+  let stat;
 
   /**
    * Set Chart X axis data (Time)
@@ -27,12 +38,21 @@ router.get('/:port/chart/packet_types_sent', function (req, res, next) {
   for (let time = min_seconds; time <= unix_time; time++) {
     x_axis_row.push(moment.unix(time).format("HH:mm:ss"));
   }
-  chart_columns.push(x_axis_row);
+  packet_loss_inbound_columns.push(x_axis_row);
+  packet_loss_outbound_columns.push(x_axis_row);
+  ping_columns.push(x_axis_row);
+  receive_bytes_columns.push(x_axis_row);
+  receive_packet_type_columns.push(x_axis_row);
+  resent_fragments_columns.push(x_axis_row);
+  resent_non_fragments_columns.push(x_axis_row);
+  resent_packets_columns.push(x_axis_row);
+  sent_bytes_columns.push(x_axis_row);
+  sent_packet_type_columns.push(x_axis_row);
+
+  const packet_groups = require('../../../app/config/packet-types.js');
 
   /**
-   * Chart data needs to line up 1:1 with the timestamps set above
-   *
-   * Loop through sent packet series
+   * Sent Packet Types
    */
   for (let packet_type in netstatListener.sent_packet_types_series_data[port]) {
     let chart_data_row = [packet_type];
@@ -42,57 +62,13 @@ router.get('/:port/chart/packet_types_sent', function (req, res, next) {
           netstatListener.sent_packet_types_series_data[port][packet_type][time] :
           null
       );
-
       chart_data_row.push(value);
     }
-
-    chart_columns.push(chart_data_row);
+    sent_packet_type_columns.push(chart_data_row);
   }
 
-  packet_groups = require('../../../app/config/packet-types.js');
-
   /**
-   * Chart data to be returned
-   * @type {{columns: Array, line: {connectNull: boolean}, x: string, groups: Array[], type: string, xFormat: string, order: string}}
-   */
-  let chart_data = {
-    columns: chart_columns,
-    groups: [packet_groups],
-    x: 'x',
-    xFormat: '%H:%M:%S',
-    type: 'bar',
-  };
-
-  res.setHeader('Content-Type', 'application/json');
-  res.send(chart_data);
-});
-
-router.get('/:port/chart/packet_types_receive', function (req, res, next) {
-  const unix_time   = Math.floor(new Date() / 1000);
-  const min_seconds = (unix_time - netstatListener.max_series_store_time);
-  const port        = req.params.port;
-
-  /**
-   * Add our listener
-   */
-  netstatListener.addListener(port);
-
-  let chart_columns = [];
-
-  /**
-   * Set Chart X axis data (Time)
-   * @type {string[]}
-   */
-  let x_axis_row = ['x'];
-  for (let time = min_seconds; time <= unix_time; time++) {
-    x_axis_row.push(moment.unix(time).format("HH:mm:ss"));
-  }
-  chart_columns.push(x_axis_row);
-
-  /**
-   * Chart data needs to line up 1:1 with the timestamps set above
-   *
-   * Loop through sent packet series
+   * Receive Packet Types
    */
   for (let packet_type in netstatListener.receive_packet_types_series_data[port]) {
     let chart_data_row = [packet_type];
@@ -102,29 +78,257 @@ router.get('/:port/chart/packet_types_receive', function (req, res, next) {
           netstatListener.receive_packet_types_series_data[port][packet_type][time] :
           null
       );
-
       chart_data_row.push(value);
     }
-
-    chart_columns.push(chart_data_row);
+    receive_packet_type_columns.push(chart_data_row);
   }
 
-  packet_groups = require('../../../app/config/packet-types.js');
+  /**
+   * Ping
+   */
+  stat = "last_ping";
+  clients = [];
+  for (let client_name in netstatListener.stat_series_data[port]) {
+    let chart_data_row = [client_name];
+
+    clients.push(client_name);
+
+    for (let time = min_seconds; time <= unix_time; time++) {
+      let value = (
+        typeof netstatListener.stat_series_data[port][client_name][stat][time] !== "undefined" ?
+          netstatListener.stat_series_data[port][client_name][stat][time] :
+          null
+      );
+      chart_data_row.push(value);
+    }
+    ping_columns.push(chart_data_row);
+  }
+
+  /**
+   * Packet Loss Inbound
+   */
+  stat = "packet_loss_in";
+  clients = [];
+  for (let client_name in netstatListener.stat_series_data[port]) {
+    let chart_data_row = [client_name];
+
+    clients.push(client_name);
+
+    for (let time = min_seconds; time <= unix_time; time++) {
+      let value = (
+        typeof netstatListener.stat_series_data[port][client_name][stat][time] !== "undefined" ?
+          netstatListener.stat_series_data[port][client_name][stat][time] :
+          null
+      );
+      chart_data_row.push(value);
+    }
+    packet_loss_inbound_columns.push(chart_data_row);
+  }
+
+  /**
+   * Packet Loss Outbound
+   */
+  stat = "packet_loss_out";
+  clients = [];
+  for (let client_name in netstatListener.stat_series_data[port]) {
+    let chart_data_row = [client_name];
+
+    clients.push(client_name);
+
+    for (let time = min_seconds; time <= unix_time; time++) {
+      let value = (
+        typeof netstatListener.stat_series_data[port][client_name][stat][time] !== "undefined" ?
+          netstatListener.stat_series_data[port][client_name][stat][time] :
+          null
+      );
+      chart_data_row.push(parseFloat(value).toFixed(2));
+    }
+    packet_loss_outbound_columns.push(chart_data_row);
+  }
+
+  /**
+   * Receive Bytes
+   */
+  stat = "receive_bytes";
+  clients = [];
+  for (let client_name in netstatListener.stat_series_data[port]) {
+    let chart_data_row = [client_name];
+
+    clients.push(client_name);
+
+    for (let time = min_seconds; time <= unix_time; time++) {
+      let value = (
+        typeof netstatListener.stat_series_data[port][client_name][stat][time] !== "undefined" ?
+          netstatListener.stat_series_data[port][client_name][stat][time] :
+          null
+      );
+      chart_data_row.push(parseFloat(value / 1024 / 1024).toFixed(2));
+    }
+    receive_bytes_columns.push(chart_data_row);
+  }
+
+  /**
+   * Send Bytes
+   */
+  stat = "sent_bytes";
+  clients = [];
+  for (let client_name in netstatListener.stat_series_data[port]) {
+    let chart_data_row = [client_name];
+
+    clients.push(client_name);
+
+    for (let time = min_seconds; time <= unix_time; time++) {
+      let value = (
+        typeof netstatListener.stat_series_data[port][client_name][stat][time] !== "undefined" ?
+          netstatListener.stat_series_data[port][client_name][stat][time] :
+          null
+      );
+      chart_data_row.push(parseFloat((value / 1024 / 1024)).toFixed(2));
+    }
+    sent_bytes_columns.push(chart_data_row);
+  }
+
+  /**
+   * Resent Packets
+   */
+  stat = "resent_packets";
+  clients = [];
+  for (let client_name in netstatListener.stat_series_data[port]) {
+    let chart_data_row = [client_name];
+
+    clients.push(client_name);
+
+    for (let time = min_seconds; time <= unix_time; time++) {
+      let value = (
+        typeof netstatListener.stat_series_data[port][client_name][stat][time] !== "undefined" ?
+          netstatListener.stat_series_data[port][client_name][stat][time] :
+          null
+      );
+      chart_data_row.push(parseFloat((value / 1024 / 1024)).toFixed(2));
+    }
+    resent_packets_columns.push(chart_data_row);
+  }
+
+  /**
+   * Resent fragments
+   */
+  stat = "resent_fragments";
+  clients = [];
+  for (let client_name in netstatListener.stat_series_data[port]) {
+    let chart_data_row = [client_name];
+
+    clients.push(client_name);
+
+    for (let time = min_seconds; time <= unix_time; time++) {
+      let value = (
+        typeof netstatListener.stat_series_data[port][client_name][stat][time] !== "undefined" ?
+          netstatListener.stat_series_data[port][client_name][stat][time] :
+          null
+      );
+      chart_data_row.push(parseFloat((value / 1024 / 1024)).toFixed(2));
+    }
+    resent_fragments_columns.push(chart_data_row);
+  }
+
+  /**
+   * Resent non-fragments
+   */
+  stat = "resent_non_fragments";
+  clients = [];
+  for (let client_name in netstatListener.stat_series_data[port]) {
+    let chart_data_row = [client_name];
+
+    clients.push(client_name);
+
+    for (let time = min_seconds; time <= unix_time; time++) {
+      let value = (
+        typeof netstatListener.stat_series_data[port][client_name][stat][time] !== "undefined" ?
+          netstatListener.stat_series_data[port][client_name][stat][time] :
+          null
+      );
+      chart_data_row.push(parseFloat((value / 1024 / 1024)).toFixed(2));
+    }
+    resent_non_fragments_columns.push(chart_data_row);
+  }
 
   /**
    * Chart data to be returned
-   * @type {{columns: Array, line: {connectNull: boolean}, x: string, groups: Array[], type: string, xFormat: string, order: string}}
    */
-  let chart_data = {
-    columns: chart_columns,
-    groups: [packet_groups],
-    x: 'x',
-    xFormat: '%H:%M:%S',
-    type: 'bar',
+  const payload = {
+    "sent_packet_types": {
+      columns: sent_packet_type_columns,
+      groups: [packet_groups],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'bar',
+    },
+    "receive_packet_types": {
+      columns: receive_packet_type_columns,
+      groups: [packet_groups],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'bar',
+    },
+    "ping": {
+      columns: ping_columns,
+      groups: [clients],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'line',
+    },
+    "packet_loss_inbound": {
+      columns: packet_loss_inbound_columns,
+      groups: [clients],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'line',
+    },
+    "packet_loss_outbound": {
+      columns: packet_loss_outbound_columns,
+      groups: [clients],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'line',
+    },
+    "receive_bytes": {
+      columns: receive_bytes_columns,
+      groups: [clients],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'line',
+    },
+    "sent_bytes": {
+      columns: sent_bytes_columns,
+      groups: [clients],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'line',
+    },
+    "resent_packets": {
+      columns: resent_packets_columns,
+      groups: [clients],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'line',
+    },
+    "resent_fragments": {
+      columns: resent_fragments_columns,
+      groups: [clients],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'line',
+    },
+    "resent_non_fragments": {
+      columns: resent_non_fragments_columns,
+      groups: [clients],
+      x: 'x',
+      xFormat: '%H:%M:%S',
+      type: 'line',
+    },
   };
 
   res.setHeader('Content-Type', 'application/json');
-  res.send(chart_data);
+  res.send(payload);
 });
 
 module.exports = router;

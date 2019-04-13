b34f552d4c19c4a00bc503aca7d0111a054cee36
Akkadius <akkadius1@gmail.com>
2019-04-12 22:08:28 -0500

    Create CLI menu

---
+++ app/cli/create-model.js
@@ -0,0 +1,43 @@
+/**
+ * @type {{check: module.exports.check}}
+ */
+module.exports = {
+  createModel: function (table) {
+    const sequelize_auto = require('sequelize-auto');
+
+    /**
+     * Load Config
+     */
+    let fs           = require('fs');
+    let eqemu_config = JSON.parse(fs.readFileSync('../eqemu_config.json', 'utf8'));
+    const database   = eqemu_config.server.database;
+
+    /**
+     * Sequelize
+     * @type {AutoSequelize}
+     */
+    let auto = new sequelize_auto(
+      database.db,
+      database.username,
+      database.password, {
+        host: database.host,
+        dialect: 'mysql',
+        directory: "models/", // prevents the program from writing to disk
+        port: '3306',
+
+        additional: {
+          timestamps: false,
+          freezeTableName: true,
+        },
+        tables: [table]
+      }
+    );
+
+    auto.run(function (err) {
+      if (err) throw err;
+
+      console.log(auto.tables); // table list
+      console.log(auto.foreignKeys); // foreign key list
+    });
+  },
+};

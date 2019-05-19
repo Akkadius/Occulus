6101394264f4ade05da068f56688f531de470e7f
Akkadius <akkadius1@gmail.com>
2019-05-18 19:49:46 -0500

    Add /server/sysinfo endpoint

--- app/routes/api/server.js
+++ app/routes/api/server.js
@@ -2,12 +2,42 @@
  * server.js
  * @type {createApplication}
  */
-let express                = require('express');
-let router                 = express.Router();
+const express              = require('express');
+const router               = express.Router();
 const serverProcessManager = require('../../core/server-process-manager')
+const os                   = require('os')
+const si                   = require('systeminformation');
 
 router.get('/hello', function (req, res, next) {
   res.send({"data": "hello"});
+});
+
+router.get('/sysinfo', async function (req, res, next) {
+  res.send(
+    {
+      "hostname": os.hostname(),
+      "uptime": os.uptime(),
+      "cpu": {
+        info: await si.cpu(),
+        speed: await si.cpuCurrentspeed(),
+        load: await si.currentLoad(),
+      },
+      "mem": await si.mem(),
+      "system": await si.system(),
+      "os": await si.osInfo(),
+      "disk": {
+        "io": await si.disksIO(),
+        "fs": {
+          "size":  await si.fsSize(),
+          "stats":  await si.fsStats(),
+        },
+      },
+      "network": {
+        "interfaces": await si.networkInterfaces(),
+        "stats": await si.networkStats(),
+      },
+    }
+  );
 });
 
 router.get('/stop', function (req, res, next) {

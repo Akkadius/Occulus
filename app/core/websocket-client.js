ffe20d42b4bcaf6fe14338202f7553f0a241afdb
Akkadius <akkadius1@gmail.com>
2019-05-20 22:17:33 -0500

    Update websocket-client.js

--- app/core/websocket-client.js
+++ app/core/websocket-client.js
@@ -4,20 +4,7 @@
 const WebSocket = require('ws');
 
 module.exports = {
-  connectionUuid : null,
   wsPort : null,
-  ws : null,
-
-  connect : async function () {
-    let self = this;
-    return new Promise(async function (resolve, reject) {
-      self.ws = new WebSocket('ws://127.0.0.1:' + self.wsPort);
-
-      self.ws.onopen = function (event) {
-        resolve(self.ws);
-      }
-    });
-  },
   getZoneOpcodeList : async function () {
     return await this.simpleGet('get_opcode_list')
   },
@@ -45,41 +32,72 @@ module.exports = {
   getZoneAttributes : async function () {
     return await this.simpleGet('get_zone_attributes')
   },
-
   setPort : function (port) {
     this.wsPort = port;
 
     return this
   },
-
   simpleGet : async function (method) {
-    var self  = this;
-    return await new Promise(async function (resolve, reject) {
-      var ws = new WebSocket('ws://127.0.0.1:' + self.wsPort);
+    var self = this;
+    try {
+      return await new Promise(async function (resolve, reject) {
 
-      ws.onopen = function (event) {
-        var connectionUuid = self.generateUUID();
-        ws.send(JSON.stringify({
-          id : connectionUuid,
-          method : 'login',
-          params : ['admin']
-        }, null, 2))
-
-        ws.send(JSON.stringify(
-          {
-            id : connectionUuid,
-            method : method,
-          }, null, 2));
-
-        ws.onmessage = function (event) {
-          var response = JSON.parse(event.data)
-          if (response && response.data && typeof response.data.status === "undefined") {
-            resolve(event.data);
-            ws.terminate()
-          }
+        if (self.wsPort === null) {
+          throw new Error('Websocket port is not set!')
         }
-      }
-    });
+
+        var ws = new WebSocket('ws://127.0.0.1:' + self.wsPort);
+
+        ws.onerror = function (event) {
+          console.log(event)
+          reject(event)
+        }
+
+        ws.on('error', (error) => {
+          console.log('client error', error);
+          reject(error)
+        });
+
+        ws.on('uncaughtException', function (err) {
+          console.log('UNCAUGHT EXCEPTION - keeping process alive:', err); // err.message is "foobar"
+        });
+
+        ws.onclose = function (event) {
+          // console.log(event)
+          reject(event)
+        }
+
+        try {
+          ws.onopen = function (event) {
+            var connectionUuid = self.generateUUID();
+            ws.send(JSON.stringify({
+              id : connectionUuid,
+              method : 'login',
+              params : ['admin']
+            }, null, 2))
+
+            ws.send(JSON.stringify(
+              {
+                id : connectionUuid,
+                method : method,
+              }, null, 2));
+
+            ws.onmessage = function (event) {
+              var response = JSON.parse(event.data)
+              if (response && response.data && typeof response.data.status === 'undefined') {
+                resolve(event.data);
+                ws.terminate()
+              }
+            }
+          }
+        } catch (e) {
+          console.log('Open' + e)
+        }
+
+      });
+    } catch (e) {
+      console.log(e)
+    }
   },
   generateUUID : function () {
     let dateTime = new Date().getTime();

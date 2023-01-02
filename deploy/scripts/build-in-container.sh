9a9a67550aae6de280ee6b0eb9b00f55badff264
mibastian <mbastian1357@gmail.com>
2023-01-02 11:04:06 +0100

    Added arm64 build. Please note the clunky fix in deploy/scripts/build-in-container.sh to not break akk-stack. I'll deal with it in an upcoming PR.

--- deploy/scripts/build-in-container.sh
+++ deploy/scripts/build-in-container.sh
@@ -25,3 +25,12 @@ cp $cwd/frontend/dist/* $cwd/public/ -R
 cd $cwd
 rm -f eqemu-admin
 pkg .
+
+###################################
+# Fixme
+# deal with name extensions when
+# building multiple architectures
+# to not break akk-stack
+###################################
+cp -a eqemu-admin-linux-x64 eqemu-admin-linux
+cp -a eqemu-admin-win-x64.exe eqemu-admin-win.exe

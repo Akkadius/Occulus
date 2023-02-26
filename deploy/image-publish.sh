979ee9971f02a251001d0b11d58c64949cc63e04
mibastian <mbastian1357@gmail.com>
2023-02-26 16:35:32 +0100

    switch docker namespace to mibastian

--- deploy/image-publish.sh
+++ deploy/image-publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build deploy/containers/eqemu-admin-build -t akkadius/eqemu-admin-build-pipeline:latest
-docker push akkadius/eqemu-admin-build-pipeline:latest
+docker build deploy/containers/eqemu-admin-build -t mibastian/eqemu-admin-build-pipeline:latest
+docker push mibastian/eqemu-admin-build-pipeline:latest
